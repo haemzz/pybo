953b1b6320bb164a264a016642f93c75d0e16c7a
haemii <117583653+haemzz@users.noreply.github.com>
2023-10-16 10:43:49 +0900

    Update settings.py

--- cofig/settings.py
+++ cofig/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(3$(8q)qy!ls7ubn&%p32_^w1)%mhaa0syorek9k3-fde$yj)^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.202.182.201']
 
 
 # Application definition
